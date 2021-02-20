b2be4ceb047d3bbd5478e25e5662b020fb452a40
Benny Zhao <benny_zhao@sfu.ca>
2021-02-20 15:53:22 -0800

    multiple screen

--- App.js
+++ App.js
@@ -1,41 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, View,  Button} from 'react-native';
 import 'react-native-gesture-handler';
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Login"
+        component={login}
+        options={{ title: 'Welcome' }}
+      />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  )
+}
+
+const login =  ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   return (
-    <View style={styles.container}>
-      <Text>Hi does this work</Text>
-      <StatusBar style="auto" />
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Email."
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setEmail(email)}
-        />
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Password."
-          placeholderTextColor="#003f5c"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        />
-      </View>
-      <View >
-        <Button title="Outline button"
-        type="outline"
-        />
-      </View>
-
-
-    </View>
+      <View style={styles.container}>
+          <Text>Hi does this work</Text>
+          <StatusBar style="auto" />
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Email."
+              placeholderTextColor="#003f5c"
+              onChangeText={(email) => setEmail(email)}
+            />
+          </View>
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              placeholder="Password."
+              placeholderTextColor="#003f5c"
+              secureTextEntry={true}w
+              onChangeText={(password) => setPassword(password)}
+            />
+          </View>
+          <View >
+            <Button title="Outline button"
+            type="outline"
+            onPress={() =>
+              navigation.navigate('Home')
+            }
+            />
+          </View>
+        </View>
   );
+}
+
+const Home =  ({ navigation }) => {
+  return(
+      <View style={styles.container}>
+        <Text>HOME PAGE</Text>
+      </View>
+  )
 }
 
 const styles = StyleSheet.create({
